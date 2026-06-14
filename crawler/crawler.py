ffd61b1e22b2412cbb5ccc18a2a846f4690a01b7
ertbateWeb <mmo1362@gmail.com>
2026-06-14 18:01:10 +0330

    Add pip install requests

---
+++ crawler/crawler.py
@@ -0,0 +1,8 @@
+import requests
+
+url = "https://edge01.155626.ir.cdn.ir/aG5p/Film/New-Server/"
+
+response = requests.get(url, timeout=30)
+
+print(f"Status Code: {response.status_code}")
+print(response.text[:1000])
