fa9a89b2c9393515c5bbdeee44323af38c1fbef4
ertbateWeb <mmo1362@gmail.com>
2026-06-15 20:34:10 +0330

    Add additional sources for DonyayeSerial in configuration

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
 SOURCES = [
-    "https://edge01.155626.ir.cdn.ir/aG5p/Film/New-Server/"
+    "https://edge01.155626.ir.cdn.ir/aG5p/Film/New-Server/",
+    # root entry
+    "https://dls2.film2cinemaha.top/DonyayeSerial/",
+
+    # known major branches (seed expansion points)
+    "https://dls2.film2cinemaha.top/DonyayeSerial/series/",
+    "https://dls2.film2cinemaha.top/DonyayeSerial/series2/",
+    "https://dls2.film2cinemaha.top/DonyayeSerial/series3/",
+    "https://dls2.film2cinemaha.top/DonyayeSerial/series4/",
+    "https://dls2.film2cinemaha.top/DonyayeSerial/movies/",
+    "https://dls2.film2cinemaha.top/DonyayeSerial/movie4/",
 ]
