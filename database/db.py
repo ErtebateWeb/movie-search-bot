17b66d7910bc72156124856189a8d4de674a966c
ertbateWeb <mmo1362@gmail.com>
2026-06-15 19:44:18 +0330

    Refactor database schema and remove existing movies database file

--- database/db.py
+++ database/db.py
@@ -4,12 +4,10 @@ import os
 DB_PATH = os.path.join("storage", "movies.db")
 
 
-# Create database connection
 def get_connection():
     return sqlite3.connect(DB_PATH)
 
 
-# Initialize database schema
 def init_db():
     conn = get_connection()
     cursor = conn.cursor()
@@ -17,10 +15,23 @@ def init_db():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS movies (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+
             title TEXT,
             year TEXT,
+
             quality TEXT,
-            url TEXT UNIQUE
+            url TEXT,
+
+            source TEXT,
+
+            imdb_id TEXT,
+
+            content_type TEXT,
+
+            season INTEGER,
+            episode INTEGER,
+
+            language TEXT
         )
     """)
 
